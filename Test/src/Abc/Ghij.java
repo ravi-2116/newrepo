96b01934b6a35e01766388823e6ffb6a56f1af46
ravi-2116 <rklogin21@gmail.com>
2022-02-05 10:57:09 +0530

    new class

---
+++ Test/src/Abc/Ghij.java
@@ -0,0 +1,16 @@
+package Abc;
+
+public class Ghij {
+
+	public void gh(int k)
+	{
+		int sum = 0;
+		
+		for (int i =0;i<=k;i++)
+		{
+			sum= sum +i;
+		}
+		
+		System.out.println("The sum of " +k +  "digits is :" +sum);
+	}
+}
---
+++ Test/src/Abc/Sum.java
@@ -0,0 +1,11 @@
+package Abc;
+
+public class Sum  extends Ghij{
+public static void main (String[] args)
+{
+	 Sum s = new Sum();
+	 s.gh(2500);
+	
+	
+}
+}
